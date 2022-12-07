9955b9ff90cb882bf4aa65b5320176cfcb7cf881
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2022-12-07 15:52:24 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by AHMED GAMAL  on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
